13f06f202921c36e4a3822eabcff9f7d9f595fa6
NurmiQ <v-kogan@rambler.ru>
2022-01-27 22:41:23 +0300

    Step 1 is done

--- app.py
+++ app.py
@@ -1,31 +1,41 @@
-from flask import Flask, request
+import json
+
+from flask import Flask, request, jsonify
 from functions import get_db
 import sqlite3
-
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def page_index():
-    data = read_json(POST_PATH)
-    return render_template('index.html', tags=get_hash(data))
-
-
-
-
-
-
-
-
-@app.route("/uploads/<path:path>")
-def static_dir(path):
-    return send_from_directory("uploads", path)
-
+@app.route('/movie/title')
+def search_movie():
+    if request.method == 'GET':
+        response = {}
+        title = request.args.get('title')
+        if title:
+            sqlite_query = f"""
+            select
+                   title,
+                   country,
+                   listed_in,
+                   release_year,
+                   description
+            from netflix
+            where title = '{title}'
+            order by release_year DESC 
+            LIMIT 1
+            """
+            result = get_db('netflix.db', sqlite_query)
+            if len(result):
+                response = {
+                    "title": result[0][0],
+                    "country" : result[0][1],
+                    "listed_in" : result[0][2],
+                    "release_year" : result[0][3],
+                    "description" : result[0][4],
+                }
+        return jsonify(response)
 
 
 if __name__ == "__main__":
     app.run()
-
-
-
--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
 import sqlite3
 
 
-def get_db(sqlite_query):
-    con = sqlite3.connect("../netflix.db")
+def get_db(db, sqlite_query):
+    con = sqlite3.connect(db)
     cur = con.cursor()
     cur.execute(sqlite_query)
     result = cur.fetchall()
     con.close()
     return result
+
+
 
