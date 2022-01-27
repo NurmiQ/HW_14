b1f5d060cb2a3bd95d4d2ea72bc80c1a12d633ae
NurmiQ <v-kogan@rambler.ru>
2022-01-27 23:58:43 +0300

    Step 2 is done

--- app.py
+++ app.py
@@ -37,5 +37,28 @@ def search_movie():
         return jsonify(response)
 
 
+@app.route('/movie/year/')
+def search_year():
+    if request.method == 'GET':
+        response = []
+        f_year = request.args.get('f_year')
+        s_year = request.args.get('s_year')
+        if f_year and s_year:
+            sqlite_query = f"""
+            select title, release_year
+            from netflix
+            where release_year between {f_year} and {s_year}
+            order by release_year DESC limit 100
+            """
+            result = get_db('netflix.db', sqlite_query)       # [(title, year), (title, year)]
+            for line in result:
+                line_dict = {
+                    'title': line[0],
+                    'release_year': line[1],
+                }
+                response.append(line_dict)
+        return jsonify(response)
+
+
 if __name__ == "__main__":
     app.run()
